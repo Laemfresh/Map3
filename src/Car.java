79e68af737c13d1f8100c24bd5e74fc96c648842
user <tahrorbek75@gmail.com>
2023-08-08 17:47:40 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car implements AutoCloseable {
+    public void drive(){
+        System.out.println("The car is running");
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Car is close");
+    }
+}
